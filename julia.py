63eeda9911810af1d1e91b3791c2baa2e8a812c2
Lukas Singer <lukas42singer (at) gmail dot com>
2016-05-27 13:05:45 +0200

    added julia.py (empty)

---
+++ julia.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Filename: julia.py
+Author: Lukas Singer <lukas42singer (at) gmail (dot) com>
+Created: 2016/05/27
+Copyright: 2016, Lukas Singer
+License: WTFPL (see COPYING or <http://www.wtfpl.net/>)
+Description: Julia set algorithm and coloring.
+"""
+
+import complex_plane
