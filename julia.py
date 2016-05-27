ec95527dec0f198e67323e9c39e99c9bd1c2c53c
Lukas Singer <lukas42singer (at) gmail dot com>
2016-05-27 15:04:59 +0200

    added julia algorithm (still laks lots of features)

--- julia.py
+++ julia.py
@@ -9,3 +9,27 @@ Description: Julia set algorithm and coloring.
 """
 
 import complex_plane
+
+def julia(width, height,
+          start, end,
+          c,
+          coloring=None,
+          bailout=2.0,
+          itermax=256):
+    cp = complex_plane.ComplexPlane(width, height, start, end)
+    if not coloring:
+        def coloring(c, z, m, i):
+            if i >= m:
+                return (0, 0, 0)
+            elif i % 2 == 0:
+                return (255, 0, 0)
+            else:
+                return (0, 0, 255)
+    for z0, px in cp:
+        zn = z0
+        i = 0
+        while abs(zn) < bailout and i < itermax:
+            zn = zn * zn + c
+            i += 1
+        cp.set_pixel(px, coloring(c, zn, itermax, i))
+    return cp
